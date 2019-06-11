93d5a7767d45dde0dd8889e6114c8c8d4679bd4f
kbsjsn <shin.kirk@gmail.com>
2019-06-10 19:59:27 -0700

    working on refactoring App

--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -4,8 +4,9 @@ var VideoListEntry = (props) => (
     <div className="media-left media-middle">
       <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
     </div>
-    <div className="media-body">
-      <div className="video-list-entry-title">{props.video.snippet.title}</div>
+    <div className="media-body">  
+      {/*FIX THIS!!!*/}
+      <div className="video-list-entry-title" onClick={this.onVideoClick.bind(this)}>{props.video.snippet.title}</div>
       <div className="video-list-entry-detail">{props.video.snippet.description}</div>
     </div>
   </div>
