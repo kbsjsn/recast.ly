3f93fe3300b6c260377490e24b14ebd645ba07ef
kbsjsn <shin.kirk@gmail.com>
2019-06-10 17:24:23 -0700

    VideoList is rendering successfully

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,10 @@
-var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
+  <div className='video-list'>
+    {props.videos.map(videoEntry =>
+      <VideoListEntry video = {videoEntry}/>
+    )}
   </div>
 );
 
